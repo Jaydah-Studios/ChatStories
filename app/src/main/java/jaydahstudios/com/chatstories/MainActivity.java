a4ec5ace89985bb06cc64f5cdedf352e6f4a3074
Jaystiqs <jayem90@gmail.com>
2017-07-15 18:34:22 +0800

    Save user progress using shared preferences. Load and continue from last point.

--- app/src/main/java/jaydahstudios/com/chatstories/MainActivity.java
+++ app/src/main/java/jaydahstudios/com/chatstories/MainActivity.java
@@ -1,6 +1,8 @@
 package jaydahstudios.com.chatstories;
 
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -13,7 +15,6 @@ import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 import jaydahstudios.com.chatstories.adapter.CustomAdapter;
@@ -21,19 +22,16 @@ import jaydahstudios.com.chatstories.database.DatabaseHelper;
 import jaydahstudios.com.chatstories.model.ChatModel;
 
 public class MainActivity extends AppCompatActivity {
-
-
     private static final String TAG = "MainActivity";
 
-//    private ListView lvProduct;
-//    private List<ChatModel> lstChat;
-    private int count = 1;
+    private SharedPreferences mPreferences;
+    private SharedPreferences.Editor mEditor;
 
+    private int count = 1; //Count for query with respect to row retreival
     private CustomAdapter adapter;
     private DatabaseHelper mDBHelper;
     private List<ChatModel> lstChat = new ArrayList<>();
     private List<ChatModel> newChat = new ArrayList<>();
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,12 +52,19 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
-////        Get product list in db when db exists
-//        chatModelList = mDBHelper.getListProduct();
-////        Init adapter
-//        adapter = new ListProductAdapter(this, chatModelList);
-////        set adapter for listview
-//        lvProduct.setAdapter(adapter);
+        // Initialize shared preferences
+        mPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        mEditor = mPreferences.edit();
+        checkSharedPreferences();
+        
+        if(count != 1 ){
+            int sPCount = count;
+            for( int i= 1; i<sPCount; i++){
+                count = i;
+                getProgress(null);
+            }
+            count++;
+        }
     }
 
     private boolean copyDatabase(Context context){
@@ -86,6 +91,7 @@ public class MainActivity extends AppCompatActivity {
     private ChatModel setUpMessage(){
         Log.d(TAG, "setUpMessage: Exec");
         return mDBHelper.getListChat(count);
+
     }
 
     public void nextClicked(View view){
@@ -99,11 +105,50 @@ public class MainActivity extends AppCompatActivity {
         lstView.setAdapter(adapter);
         adapter.notifyDataSetChanged();
 
+        Log.i(TAG, "Counter is: "+count);
+
         count++;
 
         lstView.post(new Runnable(){
             public void run() {
                 lstView.setSelection(lstView.getCount() - 1);
             }});
+    }
+    
+    public void getProgress(View view){
+        Log.d(TAG, "getProgress: Executed");
+
+        ChatModel chat = setUpMessage();
+        lstChat.add(chat);
+
+        final ListView lstView = (ListView)findViewById(R.id.listView);
+        CustomAdapter adapter = new CustomAdapter(lstChat,this);
+        lstView.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
+
+        lstView.post(new Runnable(){
+            public void run() {
+                lstView.setSelection(lstView.getCount() - 1);
+            }});
+    }
+
+    private void checkSharedPreferences(){
+        //Check if user has read to a point and has been saved from previous
+//        int counter = mPreferences.getInt("CounterSP", 1);
+        int counter = mPreferences.getInt(getString(R.string.story_progress_count), 1);
+
+        if(counter != 1){
+            count = counter;
+            Log.i(TAG, "PCounter is: "+counter);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+//        mEditor.putInt("CounterSP", count);
+        mEditor.putInt(getString(R.string.story_progress_count), count);
+        mEditor.commit();
     }
 }
