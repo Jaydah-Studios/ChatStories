6905c153df056c051b3082b6aa91d9ae26ae2a7a
Jaystiqs <jayem90@gmail.com>
2017-07-19 16:47:22 +0800

    Chaned theme colors, added custom toolbar, edited watch ad layout. UI improvements

--- app/src/main/java/jaydahstudios/com/chatstories/PromoOptionsActivity.java
+++ app/src/main/java/jaydahstudios/com/chatstories/PromoOptionsActivity.java
@@ -8,6 +8,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 /**
  * Created by Jaystiqs on 7/18/2017.
@@ -23,18 +25,47 @@ public class PromoOptionsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_promooptions);
 
-//        Button back;
+        Button shareBtn, watchAdBtn;
+//        ImageView exit;
+
         Log.d(TAG, "onCreate:  Started");
 
-//        back = (Button) findViewById(R.id.optionsBkBtn);
-//
-//        back.setOnClickListener(new View.OnClickListener() {
+        watchAdBtn = (Button) findViewById(R.id.watchAdBtnPromo);
+        shareBtn = (Button) findViewById(R.id.shareBtnPromo);
+
+        watchAdBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(PromoOptionsActivity.this, "No ads to display!", Toast.LENGTH_SHORT).show();;
+            }
+        });
+
+//        exit = (ImageView) findViewById(R.id.exitImgPromo) ;
+//        exit.setOnClickListener(new View.OnClickListener() {
 //            @Override
 //            public void onClick(View v) {
 //                Intent intent = new Intent(PromoOptionsActivity.this, MainActivity.class);
 //                startActivity(intent);
 //            }
 //        });
+
+
+        shareBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    Intent i = new Intent(Intent.ACTION_SEND);
+                    i.setType("text/plain");
+                    i.putExtra(Intent.EXTRA_SUBJECT, "Epic Chat Stories");
+                    String sAux = "\nTry this app. It has the coolest stories\n";
+                    sAux = sAux + "https://play.google.com/store/apps/details?id=Orion.Soft \n";
+                    i.putExtra(Intent.EXTRA_TEXT, sAux);
+                    startActivity(Intent.createChooser(i, "Choose one"));
+                } catch(Exception e) {
+                    //e.toString();
+                }
+            }
+        });
 
     }
 
--- app/src/main/java/jaydahstudios/com/chatstories/Splashscreen.java
+++ app/src/main/java/jaydahstudios/com/chatstories/Splashscreen.java
@@ -39,7 +39,7 @@ public class Splashscreen extends Activity {
                 try {
                     int waited = 0;
                     // Splash screen pause time
-                    while (waited < 4000) {
+                    while (waited < 3000) {
                         sleep(100);
                         waited += 100;
                     }
